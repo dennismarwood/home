a8d7bb2c384451f68294b09d40057153b937da9a
Dennis Marwood <dennismarwood@gmail.com>
2015-02-15 20:55:11 -0800

    Adding needed to path for openshfit

--- mysite/wsgi/wsgi.py
+++ mysite/wsgi/wsgi.py
@@ -8,7 +8,16 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+import sys
+#Bit frustrating, but I am not able to get openshift to include the
+#OPENSHIFT_REPO_DIR in openshfit's path. So hack around this below.
+ON_PASS = 'OPENSHIFT_REPO_DIR' in os.environ
+if ON_PASS:
+    print "wsgi.py detected PASS Environment ----------"
+    x = os.path.abspath(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'mysite'))
+    sys.path.insert(1, x)
+
+os.environ.setdefault("DJANOG_SETTINGS_MODULE", "mysite.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
